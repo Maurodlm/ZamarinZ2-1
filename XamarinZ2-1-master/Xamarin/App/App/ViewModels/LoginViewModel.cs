667e431ee849fef41d5b461caf85ae6e8aeea16d
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-05-18 09:54:48 -0400

    Commit Api y Conection

---
+++ XamarinZ2-1-master/Xamarin/App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,118 @@
+﻿namespace App.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Services;
+    using Xamarin.Forms;
+
+    public class LoginViewModel:BaseViewModel
+    {
+        #region Attributes
+        string email;
+        string password;
+        bool isrunning;
+        bool isenabled;
+        ApiService apiService;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                SetValue(ref this.email, value);
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                SetValue(ref this.password, value);
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isenabled;
+            }
+            set
+            {
+                SetValue(ref this.isenabled, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Email empty", 
+                                "Please enter your email",
+                                "Accept");
+                return;
+            }
+            if (String.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Password empty",
+                                "Please enter your password",
+                                "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var conexion = await this.apiService.CheckConnection();
+            if (!conexion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ERROR",
+                   conexion.Message,
+                   "Accept");
+                return;
+            }
+
+
+
+        }
+        #endregion
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
